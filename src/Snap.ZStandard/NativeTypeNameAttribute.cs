d6f0f51b4dbcecea3eea5a2376a2f60f5c2354e3
Lightczx <1686188646@qq.com>
2024-06-19 13:54:06 +0800

    init commit

---
+++ src/Snap.ZStandard/NativeTypeNameAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Snap.ZStandard;
+
+[AttributeUsage(AttributeTargets.All, AllowMultiple =false, Inherited = false)]
+internal sealed class NativeTypeNameAttribute : Attribute
+{
+    public NativeTypeNameAttribute(string value)
+    {
+    }
+}
